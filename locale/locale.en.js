7af52e49861a9af0005da6e6f682e38859161f5e
greywolf001au <ecowley@epcit.biz>
2013-02-27 02:34:19 +1100

    Added some extra colouring

--- locale/locale.en.js
+++ locale/locale.en.js
@@ -9,8 +9,8 @@
 var colors = require('colors')
 
 module.exports = {
-  app_version: 'Node.js IRC'.blue + ' version %version%',
-  server_start: 'Express server listening on port: ' + '%port%'.lightBlue,
+  app_version: 'Node.js IRC'.blue + ' version ' + '%version%'.blue,
+  server_start: 'Express server listening on port: ' + '%port%'.green,
   prompt: '> ',
   users: 'There are %users% users connected',
   connection_kill: 'Connection killed by admin',
